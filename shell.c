579ce94a2c7901e28ed6b50456d5b96886cec377
Moturayo <moturayoomijie123@gmail.com>
2023-10-15 11:15:13 -0700

    shell.c

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is a simple C program that prints "Hello, World!"
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    printf("Hello, World!\n");
+    return (0);
+}
